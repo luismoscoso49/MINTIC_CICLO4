c9fe532a831257cbc526f86f386264d10032c8e4
lmoscoso <luismoscoso49@gmail.com>
2022-11-02 12:00:36 -0500

    AJUSTES BACKEND

--- BACK_END/REGISTRADURIA/main.py
+++ BACK_END/REGISTRADURIA/main.py
@@ -22,136 +22,133 @@ controladorResultado= ControladorResultado()
 #CANDIDATOS
 @app.route("/candidatos", methods=['GET'])
 def getcandidatos():  # devuelve todos los candidatos
-    json = miControladorCandidato.index()
+    json = controladorCandidato.index()
     return jsonify(json)
     return "<h3>mensaje en pantalla</h3>"   #   si se qiuere devolver un mnensaje
 
-
-@app.route("/candidatos", methods=['POST'])
+@app.route("/newCandidato", methods=['POST'])
 def crearCandidato():
     data = request.get_json()   #data tiene la info del body. La informacion viene como formato JSON
-    json = miControladorCandidato.create(data)
+    json = controladorCandidato.create(data)
     return jsonify(json)
-
-
 
 @app.route("/candidatos/<string:id>", methods=['GET'])
 def getCandidato(id):
-    json = miControladorCandidato.show(id)
+    json = controladorCandidato.show(id)
     return jsonify(json)
 
 
-@app.route("/candidatos/<string:id>", methods=['PUT'])
+@app.route("/updCandidato/<string:id>", methods=['PUT'])
 def modificarCandidato(id):
     data = request.get_json()
-    json = miControladorCandidato.update(id, data)
+    json = controladorCandidato.update(id, data)
     return jsonify(json)
 
 
-@app.route("/candidatos/<string:id>", methods=['DELETE'])
+@app.route("/delCandidato/<string:id>", methods=['DELETE'])
 def eliminarCandidato(id):
-    json = miControladorCandidato.delete(id)
+    json = controladorCandidato.delete(id)
     return jsonify(json)
 
 #PARTIDOS
 @app.route("/partidos", methods=['GET'])
 def getpartidos():   #devuelve todos los partidos
-    json = miControladorPartido.index()
+    json = controladorPartido.index()
     return jsonify(json)
     return "<h3>mensaje en pantalla</h3>"   #   si se qiuere devolver un mnensaje
 
 
-@app.route("/partidos", methods=['POST'])
+@app.route("/newPartido", methods=['POST'])
 def crearPartido():
     data = request.get_json()   #data tiene la info del body. La informacion viene como formato JSON
-    json = miControladorPartido.create(data)
+    json = controladorPartido.create(data)
     return jsonify(json)
 
 
 @app.route("/partidos/<string:id>", methods=['GET'])
 def getPartido(id):
-    json = miControladorPartido.show(id)
+    json = controladorPartido.show(id)
     return jsonify(json)
 
 
-@app.route("/partidos/<string:id>", methods=['PUT'])
+@app.route("/updPartido/<string:id>", methods=['PUT'])
 def modificarPartido(id):
     data = request.get_json()
-    json = miControladorPartido.update(id, data)
+    json = controladorPartido.update(id, data)
     return jsonify(json)
 
 
-@app.route("/partidos/<string:id>", methods=['DELETE'])
+@app.route("/delPartido/<string:id>", methods=['DELETE'])
 def eliminarPartido(id):
-    json = miControladorPartido.delete(id)
+    json = controladorPartido.delete(id)
     return jsonify(json)
+
 
 #MESAS
 @app.route("/mesas", methods=['GET'])
 def getmesas():   #devuelve todos los mesas
-    json = miControladorMesa.index()
+    json = controladorMesa.index()
     return jsonify(json)
-    return "<h3>mensaje en pantalla</h3>"      #si se qiuere devolver un mnensaje
+    return "<h3>mensaje en pantalla</h3>"      #si se quiere devolver un mnensaje
 
 
-@app.route("/mesas", methods=['POST'])
+@app.route("/newMesa", methods=['POST'])
 def crearMesa():
     data = request.get_json()   #data tiene la info del body. La informacion viene como formato JSON
-    json = miControladorMesa.create(data)
+    json = controladorMesa.create(data)
     return jsonify(json)
 
 
 
 @app.route("/mesas/<string:id>", methods=['GET'])
 def getMesa(id):
-    json = miControladorMesa.show(id)
+    json = controladorMesa.show(id)
     return jsonify(json)
 
 
-@app.route("/mesas/<string:id>", methods=['PUT'])
+@app.route("/updMesa/<string:id>", methods=['PUT'])
 def modificarMesa(id):
     data = request.get_json()
-    json = miControladorMesa.update(id, data)
+    json = controladorMesa.update(id, data)
     return jsonify(json)
 
 
-@app.route("/mesas/<string:id>", methods=['DELETE'])
+@app.route("/delMesa/<string:id>", methods=['DELETE'])
 def eliminarMesa(id):
-    json = miControladorMesa.delete(id)
+    json = controladorMesa.delete(id)
     return jsonify(json)
 
 #RESULTADOS
 @app.route("/resultados", methods=['GET'])
 def getresultados():   #devuelve todos los resultados
-    json = miControladorResultado.index()
+    json = controladorResultado.index()
     return jsonify(json)
     return "<h3>mensaje en pantalla</h3>"      #si se qiuere devolver un mnensaje
 
 
-@app.route("/resultados", methods=['POST'])
+@app.route("/newResultado", methods=['POST'])
 def crearResultado():
     data = request.get_json()   #data tiene la info del body. La informacion viene como formato JSON
-    json = miControladorResultado.create(data)
+    json = controladorResultado.create(data)
     return jsonify(json)
 
 
 
 @app.route("/resultados/<string:id>", methods=['GET'])
 def getResultado(id):
-    json = miControladorResultado.show(id)
+    json = controladorResultado.show(id)
     return jsonify(json)
 
 
-@app.route("/resultados/<string:id>", methods=['PUT'])
+@app.route("/updResultado/<string:id>", methods=['PUT'])
 def modificarResultado(id):
     data = request.get_json()
-    json = miControladorResultado.update(id, data)
+    json = controladorResultado.update(id, data)
     return jsonify(json)
 
-
-@app.route("/resultados/<string:id>", methods=['DELETE'])
+@app.route("/delResultado/<string:id>", methods=['DELETE'])
 def eliminarResultado(id):
-    json = miControladorResultado.delete(id)
+    json = controladorResultado.delete(id)
     return jsonify(json)
 
 @app.route("/", methods=['GET'])
@@ -159,7 +156,6 @@ def test():
     json = {}
     json["message"] = "Server running"
     return jsonify(json)
-
 
 def loadFileConfig():  # funcion que lee archivo de conf del proyecto
     with open('config.json') as f:  # leer archivo de configuracion
