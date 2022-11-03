298e6208b26d669324ef88439e0c874fb6ef2825
lmoscoso <luismoscoso49@gmail.com>
2022-11-02 21:13:15 -0500

    CRUD FLASK

--- BACK_END/REGISTRADURIA/Repositorios/InterfaceRepositorio.py
+++ BACK_END/REGISTRADURIA/Repositorios/InterfaceRepositorio.py
@@ -2,7 +2,7 @@ import pymongo
 import certifi
 from bson import DBRef
 from bson.objectid import ObjectId
-from typing import TypeVar, Generic, List, get_origin, get_args
+from typing import TypeVar, Generic, get_args
 import json
 
 T = TypeVar('T')
@@ -10,9 +10,9 @@ T = TypeVar('T')
 class InterfaceRepositorio(Generic[T]):
     def __init__(self):
         ca = certifi.where()
-        dataconfig = self.loadFileConfig()
-        client = pymongo.MongoClient(dataconfig["data-db-connection"],tlsCAFile=ca)
-        self.baseDatos = client[dataconfig["name-db"]]
+        dataConfig = self.loadFileConfig()
+        client = pymongo.MongoClient(dataConfig["data-db-connection"], tlsCAFile=ca)
+        self.baseDatos = client[dataConfig["name-db"]]
         theClass = get_args(self.__orig_bases__[0])
         self.coleccion = theClass[0].__name__.lower()
 
@@ -54,17 +54,15 @@ class InterfaceRepositorio(Generic[T]):
         updateItem = {"$set": item}
         x = laColeccion.update_one({"_id": _id}, updateItem)
         return {"updated_count": x.matched_count}
-
     def findById(self, id):
         laColeccion = self.baseDatos[self.coleccion]
         x = laColeccion.find_one({"_id": ObjectId(id)})
         x = self.getValuesDBRef(x)
-        if x == None:
+        if x is None:
             x = {}
         else:
             x["_id"] = x["_id"].__str__()
         return x
-
     def findAll(self):
         laColeccion = self.baseDatos[self.coleccion]
         data = []
@@ -84,7 +82,6 @@ class InterfaceRepositorio(Generic[T]):
             x = self.getValuesDBRef(x)
             data.append(x)
         return data
-
     def queryAggregation(self, theQuery):
         laColeccion = self.baseDatos[self.coleccion]
         data = []
@@ -95,19 +92,21 @@ class InterfaceRepositorio(Generic[T]):
             data.append(x)
         return data
 
+
     def getValuesDBRef(self, x):
         keys = x.keys()
         for k in keys:
-        if isinstance(x[k], DBRef):
-            laColeccion = self.baseDatos[x[k].collection]
-            valor = laColeccion.find_one({"_id": ObjectId(x[k].id)})
-            valor["_id"] = valor["_id"].__str__()
-            x[k] = valor
-            x[k] = self.getValuesDBRef(x[k])
-        elif isinstance(x[k], list) and len(x[k]) > 0:
-            x[k] = self.getValuesDBRefFromList(x[k])
-        elif isinstance(x[k], dict) :
-            x[k] = self.getValuesDBRef(x[k])
+            if isinstance(x[k], DBRef):
+
+                laColeccion = self.baseDatos[x[k].collection]
+                valor = laColeccion.find_one({"_id": ObjectId(x[k].id)})
+                valor["_id"] = valor["_id"].__str__()
+                x[k] = valor
+                x[k] = self.getValuesDBRef(x[k])
+            elif isinstance(x[k], list) and len(x[k]) > 0:
+                x[k] = self.getValuesDBRefFromList(x[k])
+            elif isinstance(x[k], dict) :
+                x[k] = self.getValuesDBRef(x[k])
         return x
 
     def getValuesDBRefFromList(self, theList):
@@ -118,17 +117,15 @@ class InterfaceRepositorio(Generic[T]):
             value["_id"] = value["_id"].__str__()
             newList.append(value)
         return newList
-
     def transformObjectIds(self, x):
         for attribute in x.keys():
             if isinstance(x[attribute], ObjectId):
                 x[attribute] = x[attribute].__str__()
             elif isinstance(x[attribute], list):
                 x[attribute] = self.formatList(x[attribute])
-            elif isinstance(x[attribute], dict):
+            elif  isinstance(x[attribute], dict):
                 x[attribute]=self.transformObjectIds(x[attribute])
         return x
-
     def formatList(self, x):
         newList = []
         for item in x:
